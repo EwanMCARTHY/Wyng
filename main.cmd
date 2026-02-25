883fe923f7f523983855582f3c44a339da60656a
Ewan <ewan.maccarthy2004@gmail.com>
2026-02-25 23:49:12 +0100

    dawg

--- main.cmd
+++
@@ -1,7 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
-set PATH=c:\users\ewanm\appdata\local\programs\python\python310;%PATH%
-set PYTHONHOME=C:\Users\ewanm\AppData\Local\Programs\Python\Python310
-set NUITKA_PYTHONPATH=C:\Users\ewanm\OneDrive\Projets\Space Y\SpaceY Systems\Wyng\Wyng;C:\Users\ewanm\AppData\Local\Programs\Python\Python310\DLLs;C:\Users\ewanm\AppData\Local\Programs\Python\Python310\lib;C:\Users\ewanm\AppData\Local\Programs\Python\Python310;C:\Users\ewanm\AppData\Local\Programs\Python\Python310\lib\site-packages;C:\Users\ewanm\AppData\Local\Programs\Python\Python310\lib\site-packages\win32;C:\Users\ewanm\AppData\Local\Programs\Python\Python310\lib\site-packages\win32\lib;C:\Users\ewanm\AppData\Local\Programs\Python\Python310\lib\site-packages\Pythonwin
-"%~dp0main.exe" %*
